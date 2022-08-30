8455eee965ad45560c4f3ef31fef49e83039fffd
Dmitrii Ivanov <diman_17az@mail.ru>
2022-08-31 01:04:43 +0300

    lectures

---
+++ taskRecursion5/Program.cs
@@ -0,0 +1,22 @@
+﻿// Вычислить а в степени n
+
+// Вариант 1
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+
+
+// Вариант 2
+int PowerRec(int a, int n)
+{
+    //return n == 0 ? 1 : PowerRec(a, n - 1) * a; аналогично одной строка кода
+    if (n == 0) return 1;
+    else return PowerRec(a, n - 1) * a;
+}
+
+Console.WriteLine(PowerFor(2, 10)); // 1024
+Console.WriteLine(PowerRec(2, 10)); // 1024
+
